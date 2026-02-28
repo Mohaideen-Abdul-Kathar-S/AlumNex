72f4e7c2b0ebf497b2f41e0d5f00052fda6c1af6
Mohaideen Abdul Kathar S <mohaideenabdulkathars@gmail.com>
2026-02-28 07:13:30 +0530

    obj int

--- AlumNex_Backend/AlumNex_Backend.py
+++ AlumNex_Backend/AlumNex_Backend.py
@@ -2887,5 +2887,5 @@ def parse_id_card():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
